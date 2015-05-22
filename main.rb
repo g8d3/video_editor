d9e06ed4ff406e502f49d8c666bc49bbbca6a430
Juan Pastas <pastasjuan@gmail.com>
2015-05-22 14:40:06 -0500

    Organize methods.

--- main.rb
+++ main.rb
@@ -1,21 +1,19 @@
-# TODO organize methods... too sleepy now
-
-# split 'file.mkv', from: '1:00', to: '2:00'
-# split 'file.mkv', 60..120, 150..180
-def split(file, *times)
-	times.map do |time|
-		out = split_name file, time
-
-		split_one file, time, out
+# split 'file.mkv', 1..5, 10..15 { |command| start command }
+def split(file, *times, &block)
+	times.map  do |time|
+		start  = int_to_avconv time.begin
+		length = int_to_avconv(time.end - time.begin)
+		out    = split_name file, time
+		if block_given?
+			yield "avconv -i #{file} -ss #{start} -t #{length} #{out}", out
+		end
 	end
 end
-	
-def split_one(file, time, out)
-	start, length, final = range_to_avconv time
-	"avconv -i #{file} -ss #{start} -t #{length} #{out}"
-end
 
-def split_name(file, start, final)
+
+def split_name(file, time)
+	start = int_to_avconv time.begin
+	final = int_to_avconv time.end
 	"#{base(file)}_#{start}_#{final}#{ext(file)}"
 end
 
@@ -26,35 +24,27 @@ end
 def base(file); file[/[^.]*/] end
 def ext(file); File.extname file end
 
-# 60..100 => '00:01:00', '00:00:40', '00:01:40'
-def range_to_avconv(range)
-	[
-	int_to_avconv(range.begin),
-	int_to_avconv(range.end - range.begin),
-	int_to_avconv(range.end),
-	]
-end
-
 def int_to_avconv(int)
 	"%02i:%02i:%02i" % [int/3600, int/60 % 60, int % 60]
 end
 
-def split!(file, *times)
-	split(file, *times).map do |part|
-		puts part
-		system part
-		split_name
+def split!(file, *times, &block)
+	split file, *times do |command, out|
+		puts command
+		system command
+		yield out if block_given?
 	end
 end
 
+# merge 'file.mkv', 60..120, 150..180
 def merge(file, *times)
-	parts = split!(file, *times)
+	parts = []
+	split! file, *times do |out|
+		parts << out 
+	end
+	parts = parts.join ' + '
 	out   = merge_name(file, *times)
 	system "mkvmerge -o #{out} #{parts}"
 end
 
-p range_to_avconv 60..100
-p range_to_avconv 20..100
-split! 'test.mkv', 1..3, 5..7
-
-
+merge 'test.mkv', 1..3, 5..7
